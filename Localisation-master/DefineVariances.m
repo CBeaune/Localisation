711b188322e76dccd34e3fde3e1705945dd3a3c0
CBeaune <57994352+CBeaune@users.noreply.github.com>
2019-12-03 18:11:41 +0100

    Update DefineVariances.m

--- Localisation-master/DefineVariances.m
+++ Localisation-master/DefineVariances.m
@@ -12,18 +12,18 @@ Pinit = diag( [sigmaX^2 sigmaY^2 sigmaTheta^2] ) ;
 % Measurement noise.
 
 %Constantes nécessaires au calcul
-T = 1/200 ; %Periode d echantillonage des capteurs
-Vmax = 2 ; %Vitesse maximale admissible par le robot. A verifier. Pour l'instant on l'estime a 2m/s
+T = samplingPeriod ; %Periode d echantillonage des capteurs
+Vmax = topRobotSpeed ; %Vitesse maximale admissible par le robot. A verifier. Pour l'instant on l'estime a 2m/s
 
-sigmaXmeasurement = 1/sqrt(3)*(width/2+T*Vmax) ;  % Determined by student 
+sigmaXmeasurement = 1/sqrt(3)*(width/2+T*Vmax) ; 
 sigmaYmeasurement = 1/sqrt(3)*(width/2+T*Vmax) ;
-%10/sqrt(12) ;  % Determined by student 
-Qgamma = diag( [sigmaXmeasurement^2 sigmaYmeasurement^2] ) ;
-
+QgammaX = sigmaXmeasurement^2;
+QgammaY = sigmaYmeasurement^2;
+Qgamma=diag([QgammaX,QgammaY]);
 
 % Input noise
 
-sigmaTuning = 0.5 ; 
+sigmaTuning = 0.4 ; 
 Qwheels = sigmaTuning^2 * eye(2) ;
 Qbeta   = jointToCartesian * Qwheels * jointToCartesian.' ; 
 
@@ -33,4 +33,4 @@ Qalpha = zeros(3) ;
 
 % Mahalanobis distance threshold
 
-mahaThreshold = sqrt(chi2inv(0.95,2));  % Determined by student
+mahaThreshold = sqrt(chi2inv(0.95,2)); % Determined by student
